bed43868877c18dd5817601ad9624b5712666560
Santiago220991 <alexander.cardenas10@outlook.com>
2024-02-05 09:54:16 -0500

    feat add splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'main.dart';
+import 'package:flutter/material.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => StartState();
+}
+
+class StartState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return initScreen(context);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    var duration = const Duration(seconds: 10);
+    return Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const HomePage()));
+  }
+
+  initScreen(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          color: Colors.white
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/pokemon_logo.png"),
+            SleekCircularSlider(
+              min: 0,
+              max: 100,
+              initialValue: 100,
+              appearance: CircularSliderAppearance(
+                infoProperties: InfoProperties(
+                    mainLabelStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 25,
+                    )
+                ),
+                customColors: CustomSliderColors(
+                    dotColor: Colors.white,
+                    progressBarColor: Colors.black,
+                    shadowColor: Colors.white,
+                    trackColor: Colors.white),
+                spinnerDuration: 7,
+                animDurationMultiplier: 7,
+                animationEnabled: true,
+                startAngle: 0.0,
+                angleRange: 360,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
